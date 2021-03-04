f5be90c2ae33dea1a3284ce6b34e0814f8bfccd5
ardydj <ardydj07@gmail.com>
2021-03-04 15:07:09 -0500

    Create Sample Data for Table

---
+++ star-wars-movie-table/src/sampleTableData.js
@@ -0,0 +1,36 @@
+let sampleData = [{
+  title: 'this is a test movie',
+  movie_id: 1,
+  release_date: '2021-03-04',
+  director: 'Mr.anonymous'
+},
+{
+  title: 'this is a test movie',
+  movie_id: 1,
+  release_date: '2021-03-04',
+  director: 'Mr.anonymous'
+},
+{
+  title: 'this is a test movie',
+  movie_id: 1,
+  release_date: '2021-03-04',
+  director: 'Mr.anonymous'
+},
+{
+  title: 'this is a test movie',
+  movie_id: 1,
+  release_date: '2021-03-04',
+  director: 'Mr.anonymous'
+},
+{
+  title: 'this is a test movie',
+  movie_id: 1,
+  release_date: '2021-03-04',
+  director: 'Mr.anonymous'
+},
+{
+  title: 'this is a test movie',
+  movie_id: 1,
+  release_date: '2021-03-04',
+  director: 'Mr.anonymous'
+}];
