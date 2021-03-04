f15de9e08e092faa68c839517460fd9293a095da
ardydj <ardydj07@gmail.com>
2021-03-04 14:30:40 -0500

    Import Table Component into App Component

--- star-wars-movie-table/src/App.js
+++ star-wars-movie-table/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Table from './components/Table.js';
 
 function App() {
   return (
     <div className="App">
       <div>Hello World</div>
+      <Table />
     </div>
   );
 }
---
+++ star-wars-movie-table/src/components/Table.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Table() {
+  return (
+    <div>
+      <h1>This is from your table</h1>
+    </div>
+  );
+}
+
+export default Table;
