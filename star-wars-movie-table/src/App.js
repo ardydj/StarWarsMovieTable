738956d0820ff3e18fce48f530c9657f4980505a
ardydj <ardydj07@gmail.com>
2021-03-07 14:05:19 -0500

    Create new QueryClient and QueryClientProvider in Index.js

--- star-wars-movie-table/src/App.js
+++ star-wars-movie-table/src/App.js
@@ -1,7 +1,13 @@
 import './App.css';
 import TableComponent from './components/TableComponent.js';
+import { useQuery } from 'react-query';
+const axios = require('axios');
 
 function App() {
+  const { isLoading, isError, data, error } = useQuery('movies', () => {
+    axios.get('https://swapi.dev/api/films/')
+  });
+  console.log('this is your api data', data);
   return (
     <div className="App">
       <h1>Star Wars Movie Table</h1>
