b8be9582cfa60a763075ecb05ae87b8bdf02e67a
ardydj <ardydj07@gmail.com>
2021-03-07 17:28:51 -0500

    Make API Call For Star Wars Films with React-query

--- star-wars-movie-table/src/App.js
+++ star-wars-movie-table/src/App.js
@@ -4,9 +4,11 @@ import { useQuery } from 'react-query';
 const axios = require('axios');
 
 function App() {
-  const { isLoading, isError, data, error } = useQuery('movies', () => {
+
+  const { isLoading, isError, data, error } = useQuery('movies', () =>
     axios.get('https://swapi.dev/api/films/')
-  });
+  );
+
   console.log('this is your api data', data);
   return (
     <div className="App">
