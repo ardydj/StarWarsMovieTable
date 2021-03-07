70c59d141d70aeb2482050b720e023f3baf7df5c
ardydj <ardydj07@gmail.com>
2021-03-07 17:55:55 -0500

    Pass SW Movie Data To Each Component

--- star-wars-movie-table/src/App.js
+++ star-wars-movie-table/src/App.js
@@ -4,7 +4,6 @@ import { useQuery } from 'react-query';
 const axios = require('axios');
 
 function App() {
-
   const { isLoading, isError, data, error } = useQuery('movies', () =>
     axios.get('https://swapi.dev/api/films/')
   );
@@ -18,7 +17,7 @@ function App() {
     return (
       <div className="App">
         <h1>Star Wars Movie Table</h1>
-        <TableComponent />
+        <TableComponent movieData={data.data}/>
       </div>
     );
   }
--- star-wars-movie-table/src/components/TableComponent.js
+++ star-wars-movie-table/src/components/TableComponent.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import sampleData from '../sampleTableData.js';
 import { Table } from '@material-ui/core';
 import TableHeader from './TableHeader.js';
 import TableBodyComponent from './TableBodyComponent.js';
 
 
-function TableComponent() {
+function TableComponent(props) {
+  let { movieData } = props
+  console.log('this is your movieData: ', movieData)
   return (
     <Table>
       <TableHeader />
-      <TableBodyComponent movies={sampleData}/>
+      <TableBodyComponent movies={movieData.results}/>
     </Table>
   );
 }
