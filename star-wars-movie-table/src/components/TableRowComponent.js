462ae0f6dc59ac216960115c10e75fe25e6ab4a6
ardydj <ardydj07@gmail.com>
2021-03-07 12:33:13 -0500

    Populate Each TableCell With Props from Movie

--- star-wars-movie-table/src/components/TableRowComponent.js
+++ star-wars-movie-table/src/components/TableRowComponent.js
@@ -3,13 +3,13 @@ import { TableRow } from '@material-ui/core';
 import { TableCell } from '@material-ui/core';
 
 function TableRowComponent(props) {
-  console.log(props.movie)
+  let { movie } = props;
   return (
     <TableRow>
-      <TableCell></TableCell>
-      <TableCell></TableCell>
-      <TableCell></TableCell>
-      <TableCell></TableCell>
+      <TableCell>{movie.title}</TableCell>
+      <TableCell>{movie.movie_id}</TableCell>
+      <TableCell>{movie.release_date}</TableCell>
+      <TableCell>{movie.director}</TableCell>
     </TableRow>
   );
 }
