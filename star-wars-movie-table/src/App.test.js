5db0a6ded1d4a95a411db84e6880f12eaad4a90d
ardydj <ardydj07@gmail.com>
2021-03-08 11:56:19 -0500

    Create Test For Page Header After Data is Loaded

--- star-wars-movie-table/src/App.test.js
+++ star-wars-movie-table/src/App.test.js
@@ -19,11 +19,19 @@ function AppTestComponent() {
 //   expect(linkElement).toBeInTheDocument();
 // });
 
-test('renders a skeleton of table when loading API', async () => {
+test('renders page header when API data is loaded', async () => {
   render(<AppTestComponent />);
 
   await waitFor(() => {
-    expect(screen.getByTestId('skeleton-test').toBeInTheDocument())
+    expect(screen.getByText('Star Wars Movie Table')).toBeInTheDocument();
   });
 })
+
+// test('renders a skeleton of table when loading API', async () => {
+//   render(<AppTestComponent />);
+
+//   await waitFor(() => {
+//     expect(screen.getByText('Star Wars Movie Table').toBeInTheDocument())
+//   });
+// })
 
